93fc794aed213fcb27f194a0cb8330dabfcda4f6
ymcheung <ymcheung.tw@me.com>
2023-08-16 20:03:27 +0800

    Add navlgation links

--- constants.ts
+++ constants.ts
@@ -1,1 +1,37 @@
 export const SITENAME = '用得到的貓紋'
+
+export const categories =
+  [
+    {
+      name: 'tidy',
+      label: '收納'
+    },
+    {
+      name: 'utensils',
+      label: '廚房餐具'
+    },
+    {
+      name: 'clean',
+      label: '清潔'
+    },
+    {
+      name: 'clothes',
+      label: '衣著'
+    },
+    {
+      name: 'decoration',
+      label: '裝飾'
+    },
+    {
+      name: 'outdoor',
+      label: '戶外'
+    },
+    {
+      name: 'stationary',
+      label: '文具'
+    },
+    {
+      name: 'etc',
+      label: '還沒分類'
+    }
+  ];
