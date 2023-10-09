7aa2d2ba9c4d0341be14014bd5cba8c1800f774a
ymcheung <ymcheung.tw@me.com>
2023-10-09 21:46:55 +0800

    Disable unused import

--- src/helpers/getNumberComma.ts
+++ src/helpers/getNumberComma.ts
@@ -1,4 +1,4 @@
-import { z } from "astro:content";
+// import { z } from "astro:content";
 
 export default function getNumberComma(value: number) {
   return value?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
