eff8c260043d38a713da01d93f30250d9062f485
ymcheung <ymcheung.tw@me.com>
2023-10-09 21:43:04 +0800

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig, squooshImageService } from 'astro/config';
-import markdoc from '@astrojs/markdoc';
+import { defineConfig, squooshImageService } from "astro/config";
+import markdoc from "@astrojs/markdoc";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [markdoc()],
+  site: "https://goods.carrier.express",
+  integrations: [markdoc(), sitemap()],
   image: {
     service: squooshImageService(),
   }
