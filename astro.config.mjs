2c1f53fcf4b9feb2f9725bb727e8133db145de0a
ymcheung <ymcheung.tw@me.com>
2023-10-21 23:05:47 +0800

    Add solidjs & modular-forms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig, squooshImageService } from "astro/config";
 import markdoc from "@astrojs/markdoc";
 import sitemap from "@astrojs/sitemap";
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://goods.carrier.express",
-  integrations: [markdoc(), sitemap()],
+  integrations: [markdoc(), sitemap(), solidJs()],
   image: {
     service: squooshImageService(),
-  }
+  },
 });
