05b347e0be483381deae391f03fe500721252138
ymcheung <ymcheung.tw@me.com>
2023-11-19 10:57:43 +0800

    Create category page by graphql

---
+++ src/queries/categories.ts
@@ -0,0 +1,13 @@
+export const query = {
+  query: `
+    query {
+      allCategory(sort: { position: ASC }) {
+        slug {
+          current
+        },
+     	  title,
+        position
+      }
+    }
+  `
+};
